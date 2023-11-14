e2ace8b240c86013b2332c32a7396b64811d5f41
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2023-11-14 21:25:10 +0200

    Refactoring codes

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -23,7 +23,7 @@ const GameCardSkeleton = () => {
       <div className="animate-pulse h-2 bg-slate-700 rounded mt-3"></div>
       <div className="animate-pulse h-2 bg-slate-700 rounded mt-3"></div>
       <div className="grid grid-cols-3 gap-4 mt-4">
-         <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+        <div className="h-2 bg-slate-700 rounded col-span-2"></div>
         <div className="h-2 bg-slate-700 rounded col-span-1"></div>
       </div>
     </div>
