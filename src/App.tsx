f44bc65c6a33b825d4b084b1591b0e5303187cdd
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2023-11-19 21:16:06 +0200

    Remove unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Layout from "./components/Layout";
 
 const App = () => {
-  console.log(import.meta.env.VITE_APP_API_KEY); // Use import.meta.env instead of process.env
   return (
     <div className="bg-slate-900 text-slate-100">
       <Layout />
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_APP_API_KEY, //Api key for rawg
+    key: import.meta.env.VITE_APP_API_KEY, 
   },
 });
